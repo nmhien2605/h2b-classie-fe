48676bbc2f1fd4d6638742d146a71086c1496e2d
NMHien <nmhien2605@gmail.com>
2023-01-11 12:40:54 +0700

    test vercel

--- src/views/UserInfo.js
+++ src/views/UserInfo.js
@@ -5,7 +5,7 @@ import axios from "axios"
 const API_DOMAIN = process.env.REACT_APP_API_DOMAIN;
 const getUserInfo = async () => {
     try {
-        const { data } = await axios.get(`${API_DOMAIN}/user-info`, { withCredentials: true })
+        const { data } = await axios.get(`${API_DOMAIN}/user-info`)
         const userInfo = { ...data.user }
         // console.log(userInfo);
         localStorage.setItem("user", JSON.stringify(userInfo));
